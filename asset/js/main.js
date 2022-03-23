7913c874be5dea3cb198ed34a98ecf831b837ffc
andrea1632 <andrea.bellucci10@libero.it>
2022-03-23 14:40:09 +0100

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,18 @@
+/*
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+let contenitore = document.getElementById("contenitore");
+
+console.log(randomGeneratorInt())
+
+//funzione per generare 5 numeri random
+function randomGeneratorInt (){
+    numCpu = [];
+    for(i = 0; i < 5; i++){
+        let randomNum = Math.floor(Math.random() * 100)
+        numCpu.push(randomNum)
+    }
+    return numCpu;
+}
