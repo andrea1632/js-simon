d1b53ff98bedfa260113db26ca3b3c42a2ac4f25
andrea1632 <andrea.bellucci10@libero.it>
2022-03-23 15:04:03 +0100

    create 3 funzioni

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,8 +4,14 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto pr
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 let contenitore = document.getElementById("contenitore");
+var userNum = [];
+contenitore.innerHTML += (randomGeneratorInt())
+setTimeout(getUserNum, 100);
+arrayCompare()
+console.log(userNum) 
 
-console.log(randomGeneratorInt())
+
+
 
 //funzione per generare 5 numeri random
 function randomGeneratorInt (){
@@ -15,4 +21,23 @@ function randomGeneratorInt (){
         numCpu.push(randomNum)
     }
     return numCpu;
+}
+//funzione per ricevere 5 numeri dall'utente
+function getUserNum () {
+    for (i = 0; i < 5; i++){
+        let numInput = parseInt(prompt(`inserisci il ${i +1}° numero`))
+        userNum.push(numInput)
+    }
+    console.log(userNum)
+    return userNum;
+}
+//funzione per determinare l'uguaglianza dei 2 array
+function arrayCompare () {
+    let points = [];
+    for ( y = 0; y <= numCpu.lenght; y++ ){
+        if (numCpu.includes (userNum[y])){
+            points.push(y)
+        }
+    }
+    return points;
 }
